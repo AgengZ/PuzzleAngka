65c362439abf8b1d25294524396bc0ef4be5377e
AgengZ <agengrizqie@gmail.com>
2022-07-27 18:23:32 +0700

    Initial commit

---
+++ app/src/main/java/com/example/puzzleangka/MainActivity.java
@@ -0,0 +1,22 @@
+package com.example.puzzleangka;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflat = getMenuInflater();
+        inflat.inflate(R.menu.optionmenu,menu);
+        return true;
+    }
+}
